04c45257cd1f662ee9b06cc3aba1751000d619fb
changemaker <wns7756@naver.com>
2025-08-06 14:33:00 +0900

    호가단위 변경 업데이트 적용

--- src/lib/calcMinPriceUnit.ts
+++ src/lib/calcMinPriceUnit.ts
@@ -3,59 +3,69 @@ export function calcMinPriceUnit(price: number, market: string): number {
     return 1000;
   }
   if (price >= 1000000) {
-    return 500;
+    return 1000;
   }
   if (price >= 500000) {
-    return 100;
+    return 500;
   }
   if (price >= 100000) {
+    return 100;
+  }
+  if(price >= 50000) {
     return 50;
   }
   if (price >= 10000) {
     return 10;
   }
+  if(price >= 5000) {
+      return 5;
+  }
   if (price >= 1000) {
     return 1;
   }
   if (price >= 100) {
-    if (
-      market == "KRW-ADA" ||
-      market == "KRW-ALGO" ||
-      market == "KRW-BLUR" ||
-      market == "KRW-CELO" ||
-      market == "KRW-ELF" ||
-      market == "KRW-EOS" ||
-      market == "KRW-GRS" ||
-      market == "KRW-GRT" ||
-      market == "KRW-ICX" ||
-      market == "KRW-MANA" ||
-      market == "KRW-MINA" ||
-      market == "KRW-POL" ||
-      market == "KRW-SAND" ||
-      market == "KRW-SEI" ||
-      market == "KRW-STG" ||
-      market == "KRW-TRX"
-    ) {
-      return 1;
-    }
-    return 0.1;
+    // if (
+    //   market == "KRW-ADA" ||
+    //   market == "KRW-ALGO" ||
+    //   market == "KRW-BLUR" ||
+    //   market == "KRW-CELO" ||
+    //   market == "KRW-ELF" ||
+    //   market == "KRW-EOS" ||
+    //   market == "KRW-GRS" ||
+    //   market == "KRW-GRT" ||
+    //   market == "KRW-ICX" ||
+    //   market == "KRW-MANA" ||
+    //   market == "KRW-MINA" ||
+    //   market == "KRW-POL" ||
+    //   market == "KRW-SAND" ||
+    //   market == "KRW-SEI" ||
+    //   market == "KRW-STG" ||
+    //   market == "KRW-TRX"
+    // ) {
+    //   return 1;
+    // }
+    // return 0.1;
+    return 1;
   }
   if (price >= 10) {
-    return 0.01;
+    return 	0.1;
   }
   if (price >= 1) {
-    return 0.001;
+    return 0.01;
   }
   if (price >= 0.1) {
-    return 0.0001;
+    return 0.001;
   }
   if (price >= 0.01) {
-    return 0.00001;
+    return 0.0001;
   }
   if (price >= 0.001) {
-    return 0.000001;
+    return 0.00001;
   }
   if (price >= 0.0001) {
+    return 0.000001;
+  }
+  if(price >= 0.00001) {
     return 0.0000001;
   }
 
